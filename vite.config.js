5d257a6349e6669f9063599cc5f520c5e1a1ab5a
benjaminjalebeau <benjamin.ja.lebeau@gmail.com>
2024-10-19 23:00:24 -0600

    more file changes

--- vite.config.js
+++ vite.config.js
@@ -9,6 +9,7 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, "src/index.html"),
+        breedlist: resolve(__dirname, "src/breedlist/index.html"),
       },
     },
   },
